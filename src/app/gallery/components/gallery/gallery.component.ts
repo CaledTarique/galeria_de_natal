58f37bed552600d34f94a3b482830fadef504908
CaledTarique <caled.98@hotmail.com>
2024-11-26 18:34:48 -0300

    Adição de imagens e implementação das funções de destacar imagem, anterior e próxima

--- src/app/gallery/components/gallery/gallery.component.ts
+++ src/app/gallery/components/gallery/gallery.component.ts
@@ -1,73 +1,90 @@
-import { Component } from '@angular/core';
+import {
+  animate,
+  style,
+  transition,
+  trigger,
+  AnimationEvent,
+  state,
+} from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 import { ImageModule } from 'primeng/image';
 
+export interface Item {
+  imageSrc: string;
+  imageAlt: string;
+}
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [ImageModule ],
+  imports: [ImageModule, CommonModule],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.scss'
+  styleUrl: './gallery.component.scss',
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({ transform: 'scale(0.5)' }),
+        animate('150ms', style({ transform: 'scale(1)' })),
+      ]),
+      transition('visible => void', [
+        style({ transform: 'scale(1)' }),
+        animate('150ms', style({ transform: 'scale(0.5)' })),
+      ]),
+    ]),
+  ],
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
+  @Input() public galleryData: Item[] = [];
+  @Input() public showCount: boolean = true;
 
-  photos: any[] = [];
-  responsiveOptions: any[];
+  public previewImage: boolean = false;
+  public showMask: boolean = false;
+  public currentPreviewImage: Item = this.galleryData[0];
+  public currentindex: number = 0;
+  public controls: boolean = true;
+  public totalImageCount: number = 0;
 
-  constructor() {
-    this.responsiveOptions = [
-      {
-        breakpoint: '768px',
-        numVisible: 3,
-      }
-    ];
+  ngOnInit(): void {
+    this.totalImageCount = this.galleryData.length;
   }
 
-  ngOnInit() {
-    this.photos = [
-      {
-        itemImageSrc: 'assets/images/cesta.jpg',
-        thumbnailImageSrc: 'assets/images/cesta.jpg',
-        alt: 'Photo 1',
-        title: 'Photo 1',
-      },
-      {
-        itemImageSrc: 'assets/images/montanha.jpg',
-        thumbnailImageSrc: 'assets/images/montanha.jpg',
-        alt: 'Photo 2',
-        title: 'Photo 2',
-      },
-      {
-        itemImageSrc: 'assets/images/navio.jpg',
-        thumbnailImageSrc: 'assets/images/navio.jpg',
-        alt: 'Photo 3',
-        title: 'Photo 3',
-      },
-      {
-        itemImageSrc: 'assets/images/galleria7.jpg',
-        thumbnailImageSrc: 'assets/images/galleria7.jpg',
-        alt: 'Photo 4',
-        title: 'Photo 4',
-      },{
-        itemImageSrc: 'assets/images/galleria8.jpg',
-        thumbnailImageSrc: 'assets/images/galleria8.jpg',
-        alt: 'Photo 5',
-        title: 'Photo 5',
-      },{
-        itemImageSrc: 'assets/images/galleria9.jpg',
-        thumbnailImageSrc: 'assets/images/galleria9.jpg',
-        alt: 'Photo 6',
-        title: 'Photo 6',
-      },{
-        itemImageSrc: 'assets/images/galleria10.jpg',
-        thumbnailImageSrc: 'assets/images/galleria10.jpg',
-        alt: 'Photo 7',
-        title: 'Photo 7',
-      }
-    ];
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  public onPreviewImage(index: number): void {
+    this.showMask = true;
+    this.previewImage = true;
+    this.currentindex = index;
+    this.currentPreviewImage = this.galleryData[index];
   }
 
-  trackByFn(index: number, item: any): string {
-    return item.alt; // Use uma propriedade única para identificação
+  public onAnimationEnd(event: AnimationEvent): void {
+    if (event.toState === 'void') {
+      this.showMask = false;
+    }
   }
-  
+
+  public onClosePreview() {
+    this.previewImage = false;
+    this.showMask = false;
+  }
+
+  public nextImage(): void{
+    this.currentindex = this.currentindex + 1
+    if(this.currentindex > this.galleryData.length -1 ){
+      this.currentindex = 0;
+    }
+    this.currentPreviewImage = this.galleryData[this.currentindex]
+  }
+
+  public prevImage(): void{
+    this.currentindex = this.currentindex - 1
+    if(this.currentindex < 0){
+      this.currentindex = this.galleryData.length - 1;
+    }
+    this.currentPreviewImage = this.galleryData[this.currentindex]
+  }
+
+  public trackByFn(index: number, item: Item): string {
+    return item.imageAlt;
+  }
 }
--- src/app/gallery/home.component.ts
+++ src/app/gallery/home.component.ts
@@ -5,35 +5,129 @@ import { MenuItem } from 'primeng/api';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { AboutComponent } from './components/about/about.component';
 
-enum Esection{
+enum Esection {
   galerry = '1',
   about = '2',
+}
+
+interface Item {
+  imageSrc: string;
+  imageAlt: string;
 }
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TabMenuModule, ButtonModule, GalleryComponent,AboutComponent],
+  imports: [TabMenuModule, ButtonModule, GalleryComponent, AboutComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  items!: MenuItem[];
+  public items!: MenuItem[];
+  public data: Item[] = [
+    {
+      imageSrc: 'assets/images/cesta.jpg',
+      imageAlt: '1',
+    },
+    {
+      imageSrc: 'assets/images/montanha.jpg',
+      imageAlt: '2',
+    },
+    {
+      imageSrc: 'assets/images/navio.jpg',
+      imageAlt: '3',
+    },
+    {
+      imageSrc: 'assets/images/galleria7.jpg',
+      imageAlt: '4',
+    },
+    {
+      imageSrc: 'assets/images/galleria8.jpg',
+      imageAlt: '5',
+    },
+    {
+      imageSrc: 'assets/images/galleria9.jpg',
+      imageAlt: '6',
+    },
+    {
+      imageSrc: 'assets/images/galleria10.jpg',
+      imageAlt: '7',
+    },
+    {
+      imageSrc : 'https://images.unsplash.com/photo-1732601471612-213023f569d8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt: '8'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: '9'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1642670310920-6f4e3a3adee3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+      imageAlt: '10'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1642570517818-99c0fd6f0349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+      imageAlt: '11'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1642649149963-0ef6779df6c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: '12'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1642618215095-3523a9a36893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+      imageAlt: '13'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1642628658566-1db49cadf78c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
+      imageAlt: '14'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: '15'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=975&q=80',
+      imageAlt: '16'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
+      imageAlt: '17'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: '18'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1470770903676-69b98201ea1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: '19'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1465189684280-6a8fa9b19a7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: '20'
+    },
+    {
+      imageSrc: 'https://images.unsplash.com/photo-1506260408121-e353d10b87c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+      imageAlt: '21'
+    },
+    {imageSrc: 'https://plus.unsplash.com/premium_photo-1661766896016-16e307246d5d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt:'22'}
+    
+  ];
 
   public activeItem: MenuItem | undefined;
   public section: string | undefined = Esection.galerry;
 
   ngOnInit() {
-      this.items = [
-          { id: '1', label: 'Galeria', icon: 'pi pi-camera' },
-          { id: '2', label: 'Sobre', icon: 'pi pi-info-circle' },
-      ];
+    this.items = [
+      { id: '1', label: 'Galeria', icon: 'pi pi-camera' },
+      { id: '2', label: 'Sobre', icon: 'pi pi-info-circle' },
+    ];
 
-      this.activeItem = this.items[0];
+    this.activeItem = this.items[0];
   }
 
   onActiveItemChange(event: MenuItem) {
-      this.activeItem = event;
-      this.section = event.id;
+    this.activeItem = event;
+    this.section = event.id;
   }
 }
